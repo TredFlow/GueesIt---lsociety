a94bd076721217c17af023ba3c816d1fccbe1594
TredFlow <artsut2011@gmail.com>
2026-03-03 09:50:30 +0300

    Update guessit.py
    small changes

--- guessit.py
+++ guessit.py
@@ -10,37 +10,37 @@ def slow_print(text):
     print()
 
 num = random.randint(1, 100)
-attemps = 0
+attempts = 0
 slow_print('Welcome to the game!')
 time.sleep(1)
 tprint('GUESS IT')
 time.sleep(2)
 while True:
     guess = input('Enter a number between 1 and 100:')
-    attemps += 1
+    attempts += 1
     if not guess.isdigit(): print("Enter the NUMBERS, bro"); continue
     i = int(guess)
     if i < 1 or i > 100: 
         slow_print('The number must be between 1 and 100!'); continue
     if i == num:
-        if attemps == 1:
-            slow_print(f'WTF! Are you Einstein? Even I can't guess in {attemps} attempts.')
-        elif attemps <=7:
+        if attempts == 1:
+            slow_print(f'WTF! Are you Einstein? Even I can`t guess in {attempts} attempts.')
+        elif attempts <=7:
             slow_print(f'Bravo boss! {attempts} attempts, a worthy result.')
-        elif attemps <=10:
-            slow_print(f'More or less, {attemps} attempts, but still congratulations!')
-        elif attemps >= 11:
+        elif attempts <=10:
+            slow_print(f'More or less, {attempts} attempts, but still congratulations!')
+        elif attempts >= 11:
             slow_print('Hey, dude! When did you get your IQ tested?') 
             time.sleep(1)
             slow_print('This is a cool neighborhood, get lost!') 
             time.sleep(1)
-            slow_print(f'{attemps} attempts, shame!')
+            slow_print(f'{attempts} attempts, shame!')
             time.sleep(1)
-            slow_print('We're calling your mom!')
+            slow_print('We`re calling your mom!')
             time.sleep(1)
             tprint('BEEP')
             time.sleep(1)
-            slow_print('Your mom: Are you serious? I'll give him up right now.')
+            slow_print('Your mom: Are you serious? I`ll give him up right now.')
             time.sleep(1)
             tprint('HAHAHAHA')
             time.sleep(1)
@@ -52,8 +52,10 @@ while True:
     elif i > num:
         slow_print('The number is less!')
 
+
     
     
     
+
 
 
