029eb6749a6ad20be6dadeab51684ff626a8a7bf
TredFlow <artsut2011@gmail.com>
2026-03-03 18:19:51 +0300

    Update guessit.py
    Refactor: Adding the modguess.py module

--- guessit.py
+++ guessit.py
@@ -1,14 +1,6 @@
 import random
 import time
-from art import tprint
-import sys
-def slow_print(text):
-    for char in text:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(0.05) 
-    print()
-
+from modguess import slow_print, messages, tprint
 num = random.randint(1, 100)
 attempts = 0
 slow_print('Welcome to the game!')
@@ -18,44 +10,21 @@ time.sleep(2)
 while True:
     guess = input('Enter a number between 1 and 100:')
     attempts += 1
-    if not guess.isdigit(): print("Enter the NUMBERS, bro"); continue
+    if not guess.isdigit(): 
+        print("Enter the NUMBERS, bro")
+        continue
     i = int(guess)
     if i < 1 or i > 100: 
-        slow_print('The number must be between 1 and 100!'); continue
+        slow_print('The number must be between 1 and 100!')
+        continue
     if i == num:
-        if attempts == 1:
-            slow_print(f'WTF! Are you Einstein? Even I can`t guess in {attempts} attempts.')
-        elif attempts <=7:
-            slow_print(f'Bravo boss! {attempts} attempts, a worthy result.')
-        elif attempts <=10:
-            slow_print(f'More or less, {attempts} attempts, but still congratulations!')
-        elif attempts >= 11:
-            slow_print('Hey, dude! When did you get your IQ tested?') 
-            time.sleep(1)
-            slow_print('This is a cool neighborhood, get lost!') 
-            time.sleep(1)
-            slow_print(f'{attempts} attempts, shame!')
-            time.sleep(1)
-            slow_print("We're calling your mom!")
-            time.sleep(1)
-            tprint('BEEP')
-            time.sleep(1)
-            slow_print("Your mom: Are you serious? I'll give him up right now.")
-            time.sleep(1)
-            tprint('HAHAHAHA')
-            time.sleep(1)
-            tprint('DONT CRY')
-
+        slow_print(messages(attempts))
         break
     elif i < num:
         slow_print('The number is higher!')
     elif i > num:
         slow_print('The number is less!')
 
-
-    
-    
-    
 
 
 
