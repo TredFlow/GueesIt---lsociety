bfee8a4b9b7f3a243cfb01446644605e165f59be
TredFlow <artsut2011@gmail.com>
2026-03-04 11:37:51 +0300

    Update guessit.py
    Added a high-score system via guessrecord, minor tweaks

--- guessit.py
+++ guessit.py
@@ -1,12 +1,16 @@
 import random
 import time
 from modguess import slow_print, messages, tprint
+from guessrecord import record_check, record_guess
+best_score = record_check()
 num = random.randint(1, 100)
 attempts = 0
 slow_print('Welcome to the game!')
 time.sleep(1)
 tprint('GUESS IT')
 time.sleep(2)
+slow_print(f"Current record to beat: {best_score} attempts")
+time.sleep(1)
 while True:
     guess = input('Enter a number between 1 and 100:')
     attempts += 1
@@ -18,12 +22,15 @@ while True:
         slow_print('The number must be between 1 and 100!')
         continue
     if i == num:
-        slow_print(messages(attempts))
+        messages(attempts)
         break
     elif i < num:
         slow_print('The number is higher!')
     elif i > num:
         slow_print('The number is less!')
+
+record_guess(attempts, best_score)
+
 
 
 
