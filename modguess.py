1bb82a4646681131eeb626559d23c9e5bab94a4d
TredFlow <artsut2011@gmail.com>
2026-03-05 07:55:58 +0300

    Update modguess.py
    Added localization to the languess module.

--- modguess.py
+++ modguess.py
@@ -1,33 +1,34 @@
 import sys
 import time
 from art import tprint
-
+from languess import Translations
 def slow_print(text):
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(0.05) 
     print()
-def messages(attempts):
+def messages(attempts, lang):
     if attempts == 1:
-            slow_print(f'WTF! Are you Einstein? Even I can`t guess in {attempts} attempts.')
+            slow_print(Translations [lang]['attempts_1'].format(attempts))
     elif attempts <= 7:
-            slow_print(f'Bravo boss! {attempts} attempts, a worthy result.')
+        slow_print(Translations [lang]['attempts_7'].format(attempts))
     elif attempts <= 10:
-            slow_print(f'More or less, {attempts} attempts, but still congratulations!')
+            slow_print(Translations [lang]['attempts_10'].format(attempts))
     elif attempts >= 11:
-            slow_print('Hey, dude! When did you get your IQ tested?') 
+            slow_print(Translations [lang]['troll_start']) 
             time.sleep(1)
-            slow_print('This is a cool neighborhood, get lost!') 
+            slow_print(Translations [lang]['hood_troll']) 
             time.sleep(1)
-            slow_print(f'{attempts} attempts, shame!')
+            slow_print(Translations [lang]['shame'].format(attempts))
             time.sleep(1)
-            slow_print("We're calling your mom!")
+            slow_print(Translations [lang]['calling'])
             time.sleep(1)
             tprint('BEEP')
             time.sleep(1)
-            slow_print("Your mom: Are you serious? I'll give him up right now.")
+            slow_print(Translations [lang]['mom_answer'])
             time.sleep(1)
             tprint('HAHAHAHA')
             time.sleep(1)
             tprint('DONT CRY')
+
