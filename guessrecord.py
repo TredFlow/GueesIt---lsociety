11ea7141f129804df987e54eb1e48d518ee45148
TredFlow <artsut2011@gmail.com>
2026-03-06 07:35:45 +0300

    Update guessrecord.py
    Changed 'record_mod' and added string parsing to 'record_check'

--- guessrecord.py
+++ guessrecord.py
@@ -1,29 +1,35 @@
 import os
 from art import tprint
+from datetime import datetime
 from languess import Translations
+now = datetime.now().strftime("%Y-%d-%m %H:%M")
+
 def record_check():
     file_name = 'record.txt'
     if  not os.path.exists(file_name ):
         with open ('record.txt', 'w') as f:
-          f.write(str(100))
-        return(100)
+          f.write('System | 100 | 1970-01-01 00:00\n')
+        return 100
     with open(file_name, 'r') as f:
-        content = f.read().strip()
-        return int(content) if content else 100
+        lines = f.readlines()
+        last_line = lines[-1].strip()
+        part = last_line.split('|')
+        best_score = int((part[1]).strip())
+        return best_score
+    return 100
     
-def record_mod(new_record):
-    with open ('record.txt', 'a') as f:
-       f.write(str(new_record) + '\n') 
-
-def record_guess(attempts, current_record, lang):
-    attempts = int(attempts)
+def record_mod(name, attempts, now):
+    with open ('record.txt', 'a', encoding="utf-8") as f:
+        f.write(f'{name} | {attempts} | {now}\n')
+    
+def record_guess(name, attempts, current_record, lang):
     current_record = int(current_record)
     if attempts == current_record:
         print(Translations [lang]['quote'])
         tprint(Translations [lang]['look'])
     elif attempts < current_record:
         print(Translations [lang]['new_record'].format(current_record - attempts))
-        record_mod
+        record_mod(name, attempts, now)
     elif attempts > current_record:
         print(Translations [lang]['bad_record'].format(current_record))
         
